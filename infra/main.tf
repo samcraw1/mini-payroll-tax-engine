63add30ea4573bb5038a98b6a165933299fe83ac
samcraw1 <samcraw01@gmail.com>
2026-08-18 23:01:39 -0400

    linked mysql to aws

--- infra/main.tf
+++ infra/main.tf
@@ -21,3 +21,19 @@ resource "aws_s3_bucket" "my_bucket" {
 output "random_bucket_name"{
     value = aws_s3_bucket.my_bucket.bucket
 }
+
+resource "aws_db_instance" "payroll_db" {
+    allocated_storage = 20
+    engine = "mysql"
+    engine_version = "8.0"
+    instance_class = "db.t3.micro"
+    username = "root"
+    password = "yourpassword"
+    skip_final_snapshot = true
+}
+
+output "database_endpoint" {
+    value = aws_db_instance.payroll_db.endpoint
+}
+
+
